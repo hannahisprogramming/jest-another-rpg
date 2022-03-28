7e78c8d4294027b5027cfa5a27ddd575976ffe7c
Hannah Bush <heb.rutgers@gmail.com>
2022-03-27 22:35:10 -0400

    get health, check if alive, and reduce health added

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,5 +1,3 @@
-const { expect } = require('@jest/globals');
-const { test } = require('picomatch');
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 
@@ -50,4 +48,17 @@ test('checks if player is alive or not', () => {
   player.health = 0;
 
   expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+  const player = new Player('Dave');
+  const oldHealth = player.health;
+
+  player.reduceHealth(5);
+
+  expect(player.health).toBe(oldHealth - 5);
+
+  player.reduceHealth(99999);
+
+  expect(player.health).toBe(0);
 });
